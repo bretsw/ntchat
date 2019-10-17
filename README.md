d836f7c73a2af88ece82f9eac6287c2ad1355e11
Bret Staudt Willet <bret.staudt.willet@gmail.com>
2019-10-17 01:05:37 -0400

    updated slide structure

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# ntchat
+# #ntchat
+
+This is a repository for:
+
+*How Do You Do, Fellow Kids? “New Teachers” in Twitter #ntchat*  
+a presentation at AECT's Annual International Convention in Las Vegas, NV  
+Tuesday, 10/22/2019, 8:00am
+
+- [K. Bret Staudt Willet](http://bretsw.com/), Michigan State University
+- [Lauren M. Bagdy](https://researchgate.net/profile/Lauren_Bagdy/), Florida State University
+
+**Presentation slides: https://bretsw.github.io/ntchat/**
+
+## Description
+
+This presentation describes participation in Twitter #ntchat (i.e., “new teacher chat”) using a mix of quantitative methods and qualitative content analysis. Surprisingly, results showed that new teachers are not the main contributors to #ntchat. This session will be of interest to researchers studying online learning and anyone working to support teachers’ or instructional design and technology (IDT) professionals’ growth. Presenters will facilitate discussion among attendees regarding various online spaces for supporting professional development and methods for studying them.
+
+## Keywords
+
+social media; teacher education
+
+## Extended Abstract
+
+### Introduction and Background
+
+This proposal reports a Category 2 work-in-progress on new teachers’ online professional development; specifically, the purpose of this presentation is to describe participation in Twitter #ntchat (i.e., “new teacher chat”). 
+
+Since its inception in 2006, Twitter has gained momentum as an online platform for educators’ professional development. In a study by Visser, Evering, and Barrett (2014), teachers reported using Twitter for professional use more often than personal use. Carpenter and Krutka (2015) found that teachers enjoy using Twitter for professional growth and development because it allows them to stay on top of educational trends and connect with educators outside of their face-to-face network. 
+
+One way teachers can connect on Twitter is through Twitter “chats,” which Carpenter and Krutka (2014) defined as: “synchronous events where many users participate in a discussion via tweets on a specific topic, usually using a predetermined hashtag at a designated time” (p. 12). In particular, one education-related Twitter chat, #ntchat, was started in 2010 “to provide weekly mentoring and focus on the needs of new, new to the profession and pre-service teachers.” Synchronous discussions on #ntchat occur Wednesdays from 8:00-9:00pm EST; however, Twitter users can post using the hashtag anytime.  Research has shown that online spaces like #ntchat provide new teachers opportunities for induction (Zuidema, 2012), empathetic support (Hur & Brush, 2009), and mentoring (Smith Risser, 2013)—all of which are critical for new teachers. 
+
+The purpose of this study was to examine Twitter #ntchat to understand more about participants and how they are using the space. Specifically, two research questions guided this study: RQ1. What are participants doing in #ntchat? RQ2. Who is contributing to #ntchat? 
+
+### Method and Results
+
+Tweets containing the text “#ntchat” were collected with a Twitter Archiving Google Sheet (Hawksey, 2014) during a period of nine months, from May 1, 2018 to January 31, 2019. More complete metadata (e.g., tweet type, tweeter profile information) were obtained using the “rtweet” package (Kearney, 2018) in R. Subsequent statistical analyses were also completed using R.
+
+### RQ1. What are participants doing in #ntchat? 
+During the nine months of #ntchat tweets, 11,735 different tweeters created 28,098 unique tweets. To focus on what new teachers might be contributing to #ntchat, “retweets” (i.e., sharing a tweet to one’s own Twitter timeline, which could be considered more a form of rebroadcasting than original contribution) were removed from the dataset. The remaining tweets (minus retweets) roughly fell into categories of “original posts” and “replies;” the new dataset contained 7,596 (28.32% of all tweets) unique tweets from 992 (8.45% of all tweeters) different tweeters. The mean contribution of tweets per tweeter was 8.02 (SD=44.30), with a median of one. Approximately half (53.12%) of contributors only tweeted to #ntchat once in nine months, and 89.31% tweeted nine times or less—meaning 10.69% tweeted an average of monthly. In terms of engagement with #ntchat posts and replies, approximately half (54.85%) were retweeted; the mean retweets per tweet was 2.81 (SD=17.18). More than two-thirds (69.33%) of #ntchat posts and replies received “likes;” the mean likes per tweet was 5.77 (SD=35.14). 
+
+To conduct content analysis, 200 tweets were randomly sampled from #ntchat posts and replies. An “a priori” coding scheme (Authors, in revision) was used to categorize groups of tweets related to: (a) “self,” such as promoting one’s own work; (b) “others,” such as highlighting the work of peers; (c) “mutual engagement,” such as discussing ideas or collaborating; and (d) “miscellaneous,” which included anything not captured by the first three categories—such as news, didactic content, and tweets that were off topic. Approximately half (49.00% ± 9.61%—the margin of error represents a 95% confidence interval) of the #ntchat sample tweets were related to self, approximately one-third (36.00% ± 9.23%) were related to others, 15.00% ± 6.86% were related to mutual engagement, and none were categorized as miscellaneous. The percent agreement between the two coders was 84.50%, and Cohen’s kappa was .75—suggesting substantial agreement (Landis & Koch, 1977).
+
+### RQ2. Who is contributing to #ntchat? 
+Using the same random sample of 200 tweets as above, 104 tweeters were identified for content analysis of their Twitter profiles. Self-identified pre-service and newly in-service teachers rarely contributed to #ntchat (5.77% ± 4.48%); neither did teacher educators (2.88% ± 3.21%). The most significant categories of contributors to #ntchat were educational technology companies and organizations (35.58% ± 9.20%) and educational consultants, speakers, authors, and trainers (31.73% ± 8.95%). Broad categories of “teacher” (22.12% ± 7.98%) and “administrator” (11.54% ± 6.14%) were also present. Most Twitter profiles were able to be coded—only 1.92% ± 2.64% were placed in the “other/unknown” category. Because these categories were not mutually exclusive, IRR scores were calculated for each category. Percent agreement ranged from 91.35% to 98.08% and Cohen’s kappa scores ranged from .71 to .96—substantial to almost perfect agreement (Landis & Koch, 1977).
+
+### Conclusion
+
+Perhaps unsurprisingly, Twitter #ntchat demonstrated “participation inequality” common to online social spaces. More unexpected was how infrequently users self-identifying as “new teachers” contributed to new teacher chat. Instead, #ntchat seemed to be dominated by users—largely ed tech companies, organizations, and consultants—promoting their own “brand” content far more than interacting with new teachers. Teachers might still find benefit from #ntchat as a content source, but less likely as a space to interact with peers or receive active mentoring.
+
+The strength of this study’s approach is that there were few initial assumptions about what users are doing in #ntchat—a multiple methods approach offered more insight than looking at descriptive statistics or tweet content alone. Researchers might find this study’s combination of quantitative methods and qualitative content analysis to be useful, but future research should also include surveys or interviews to provide a deeper understanding of how the participants perceive benefits of #ntchat.
---
+++ ntchat_identity.R
@@ -0,0 +1,361 @@
+## --------------------------------------------------------------
+## load libraries
+## --------------------------------------------------------------
+
+library(tidyverse)  # for data manipulation; includes library(dplyr); library(ggplot2)
+library(stringr)  # for ease of working with string and character varbiables
+library(rtweet)
+library(anytime); library(lubridate)  # for working with dates
+library(igraph)  # for processing social network
+library(ggraph)  # for visualizing social network
+library(irr)
+
+## --------------------------------------------------------------
+## load, clean, view data
+## --------------------------------------------------------------
+
+start_date <- "05-01-2018 00:00:00 EST" %>% mdy_hms %>% as_datetime
+end_date <- "10-01-2019 00:00:00 EST" %>% mdy_hms %>% as_datetime  ## was 02-01-2019
+
+
+file_list <- stringr::str_extract_all(dir("data"), "^ntchat_raw\\S+", simplify=TRUE)
+file_list <- file_list[file_list[,1] != "", ]
+file_list <- paste0("data/", filelist)
+
+
+ntchat_all <- lapply(file_list, read.csv, header=TRUE, colClasses='character')
+ntchat_all %>% sapply(dim)
+
+
+## --------------------------------------------------------------
+## pull full tweet content using library(rtweet)
+## --------------------------------------------------------------
+
+## See https://rtweet.info/ for details on library(rtweet)
+## See https://rtweet.info/reference/index.html for all rtweet functions
+## See https://apps.twitter.com/ for details on Twitter developer application
+
+statuses <- ntchat_all[[1]] %>% as.data.frame %>% pull(id_str)
+
+ntchat_rtweet <- statuses %>% lookup_tweets %>% flatten  # Returns data on up to 90,000 Twitter statuses. 
+#ntchat_rtweet %>% dim
+#ntchat_rtweet %>% write.csv("data/ntchat_rtweet.csv", row.names=FALSE)
+
+
+## --------------------------------------------------------------
+## 
+## --------------------------------------------------------------
+
+ntchat_rtweet <- read.csv("data/ntchat_rtweet.csv", header=TRUE, colClasses='character')
+#ntchat_rtweet %>% names
+
+ntchat <- ntchat_rtweet %>% as.data.frame %>%
+        distinct(status_id, .keep_all = TRUE) %>%
+        filter(status_id != "",
+               !is.na(status_id),
+               user_id != "",
+               user_id != "[deleted]",
+               text != "[deleted]",
+               text != "[removed]",
+               text != "",
+               lang=="en"
+               ) %>%
+        mutate(date_time = {created_at %>% ymd_hms %>% as_datetime}) %>%
+        select(-created_at) %>% 
+        filter((date_time > start_date) & (date_time < end_date))
+
+
+## --------------------------------------------------------------
+## Raw Numbers
+## --------------------------------------------------------------
+
+n_tweets <- ntchat %>% pull(status_id) %>% unique %>% length
+n_tweeters <- ntchat %>% pull(user_id) %>% unique %>% length
+paste("Number of tweets", n_tweets); paste("Number of tweeters:", n_tweeters)
+
+## --------------------------------------------------------------
+## Frequencies
+## --------------------------------------------------------------
+
+tweets_table <- ntchat %>% pull(user_id) %>%
+        table %>% as.data.frame %>%
+        rename(user = ".", all_tweets = "Freq") %>%
+        arrange(desc(all_tweets))
+head(tweets_table, 10)
+
+tweets_table$all_tweets %>% mean %>% round(2)
+tweets_table$all_tweets %>% sd %>% round(2)
+tweets_table$all_tweets %>% median
+tweets_table$all_tweets %>% min
+tweets_table$all_tweets %>% max
+
+tweeter_freq_table <- tweets_table$all_tweets %>% table %>% as.data.frame %>%
+        rename(number_of_tweets = ".", number_of_tweeters = "Freq") %>%
+        mutate(percentage_of_tweeters = ((number_of_tweeters / n_tweeters) * 100) %>% round(2))
+head(tweeter_freq_table, 10)
+
+paste0(sum(tweeter_freq_table$percentage_of_tweeters[1:10]), 
+       "% of #ntchat tweeters tweeted 10 times or less.")
+
+
+## --------------------------------------------------------------
+## Dates
+## --------------------------------------------------------------
+
+## 9 months: May 1, 2018 - Jan 31, 2019
+max(ntchat$date_time) %>% difftime(min(ntchat$date_time), units="days")
+max(ntchat$date_time) %>% difftime(min(ntchat$date_time), units="weeks")
+
+
+## --------------------------------------------------------------
+## Raw Numbers - No Retweets
+## --------------------------------------------------------------
+
+## first, filter out all RTs
+ntchat_no_rt <- ntchat %>% filter(is_retweet==FALSE)
+ntchat_no_rt %>% dim
+
+## percentage of tweets that are not retweets
+n_no_rt <- ntchat_no_rt %>% pull(status_id) %>% unique %>% length
+n_no_rt 
+paste0(((n_no_rt / n_tweets) * 100) %>% round(2),
+       "% of #ntchat tweets that are not retweets.")
+
+## percentage of tweeters that contributed something other than retweets
+n_tweeters_no_rt <- ntchat_no_rt %>% pull(user_id) %>% unique %>% length
+n_tweeters_no_rt
+paste0({(n_tweeters_no_rt / n_tweeters) * 100} %>% round(2),
+       "% of #ntchat tweeters contributed something other than retweets.")
+
+
+## --------------------------------------------------------------
+## Frequencies - No Retweets
+## --------------------------------------------------------------
+
+tweets_no_rt_table <- ntchat_no_rt %>% pull(user_id) %>%
+        table %>% as.data.frame %>%
+        rename(user = ".", all_tweets = "Freq") %>%
+        arrange(desc(all_tweets))
+head(tweets_no_rt_table, 10)
+
+## average tweets per tweeter
+tweets_no_rt_table$all_tweets %>% mean %>% round(2)
+tweets_no_rt_table$all_tweets %>% sd %>% round(2)
+tweets_no_rt_table$all_tweets %>% median
+tweets_no_rt_table$all_tweets %>% min
+tweets_no_rt_table$all_tweets %>% max
+
+tweeter_no_rt_freq_table <- tweets_no_rt_table$all_tweets %>% table %>% as.data.frame %>%
+        rename(number_of_tweets = ".", number_of_tweeters = "Freq") %>%
+        mutate(percentage_of_tweeters = ((number_of_tweeters / n_tweeters_no_rt) * 100) %>% round(2))
+head(tweeter_no_rt_freq_table, 10)
+
+## tweeters (more than retweets) who only contributed once
+paste0(tweeter_no_rt_freq_table[1, 'number_of_tweeters'], 
+       "% of #ntchat tweeters who only contributed once")
+
+## tweeters (more than retweets) who contributed, an avearage, once per month or less
+paste0(sum(tweeter_no_rt_freq_table$percentage_of_tweeters[1:10]), 
+       "% of #ntchat tweeters tweeted 10 times or less.")
+
+## average number of retweets per tweet
+ntchat_no_rt$retweet_count %>% as.numeric %>% mean %>% round(2)
+ntchat_no_rt$retweet_count %>% as.numeric %>% sd %>% round(2)
+ntchat_no_rt$retweet_count %>% as.numeric %>% median %>% round(2)
+ntchat_no_rt$retweet_count %>% as.numeric %>% range %>% round(2)
+
+number_of_retweets_table <- ntchat_no_rt %>% pull(retweet_count) %>%
+        table %>% as.data.frame %>%
+        rename(retweets = ".", count = "Freq") %>%
+        arrange(desc(count))
+n_zero_retweets <- number_of_retweets_table$count[1]
+paste0({(n_zero_retweets / n_no_rt) * 100} %>% round(2),
+       "% of #ntchat tweets were not retweeted")
+
+## average number of likes per tweet
+ntchat_no_rt$favorite_count %>% as.numeric %>% mean %>% round(2)
+ntchat_no_rt$favorite_count %>% as.numeric %>% sd %>% round(2)
+ntchat_no_rt$favorite_count %>% as.numeric %>% median %>% round(2)
+ntchat_no_rt$favorite_count %>% as.numeric %>% range %>% round(2)
+
+number_of_favorites_table <- ntchat_no_rt %>% pull(favorite_count) %>%
+        table %>% as.data.frame %>%
+        rename(retweets = ".", count = "Freq") %>%
+        arrange(desc(count))
+n_zero_favorites <- number_of_favorites_table$count[1]
+paste0({(n_zero_favorites / n_no_rt) * 100} %>% round(2),
+       "% of #ntchat tweets were not liked")
+
+## average time on Twitter per tweeter (in years)
+ntchat_no_rt %>% distinct(user_id, .keep_all = TRUE) %>%
+        mutate(twitter_start = (account_created_at %>% ymd_hms %>% as_datetime),
+               duration = (max(ntchat_no_rt$date_time) %>% 
+                               difftime(twitter_start, units="days") %>% 
+                               as.numeric / 365
+                           ) %>% round(2)
+               ) %>%
+        summarize(mean_duration = duration %>% mean, 
+                  sd_duration = duration %>% sd,
+                  median_duration = duration %>% median, 
+                  min_duration = duration %>% min,
+                  max_duration = duration %>% max
+                  )
+
+## average number of followers per tweeter
+ntchat_no_rt %>% distinct(user_id, .keep_all = TRUE) %>%
+        mutate(followers_count = followers_count %>% as.numeric) %>% 
+        summarize(mean_followers = followers_count %>% mean, 
+                  sd_followers = followers_count %>% sd,
+                  median_followers = followers_count %>% median, 
+                  min_followers = followers_count %>% min,
+                  max_followers = followers_count %>% max
+                  )
+
+## average number of friends per tweeter
+ntchat_no_rt %>% distinct(user_id, .keep_all = TRUE) %>%
+        mutate(friends_count = friends_count %>% as.numeric) %>% 
+        summarize(mean_friends = friends_count %>% mean, 
+                  sd_friends = friends_count %>% sd,
+                  median_friends = friends_count %>% median, 
+                  min_friends = friends_count %>% min,
+                  max_friends = friends_count %>% max
+                  )
+
+## average number of tweets per tweeter (for the duration of their time on Twitter)
+ntchat_no_rt %>% distinct(user_id, .keep_all = TRUE) %>%
+        mutate(statuses_count = statuses_count %>% as.numeric) %>% 
+        summarize(mean_statuses = statuses_count %>% mean, 
+                  sd_statuses = statuses_count %>% sd,
+                  median_statuses = statuses_count %>% median, 
+                  min_statuses = statuses_count %>% min,
+                  max_statuses = statuses_count %>% max
+                  )
+
+## average number of likes per tweeter
+ntchat_no_rt %>% distinct(user_id, .keep_all = TRUE) %>%
+        mutate(favourites_count = favourites_count %>% as.numeric) %>% 
+        summarize(mean_favourites = favourites_count %>% mean, 
+                  sd_favourites = favourites_count %>% sd,
+                  median_favourites = favourites_count %>% median, 
+                  min_favourites = favourites_count %>% min,
+                  max_favourites = favourites_count %>% max
+                  )
+
+
+## --------------------------------------------------------------
+## Sample for human coding
+## --------------------------------------------------------------
+
+set.seed(02182019)
+sample_ids <- ntchat_no_rt %>% pull(status_id) %>% sample(size=200)
+sample_ids %>% head
+
+ntchat_tweet_sample <- ntchat_no_rt %>% filter(status_id %in% sample_ids) %>%
+        mutate(self=0, others=0, mutual=0, misc=0) %>%
+        select(status_id, user_id, date_time, screen_name, text, status_url,
+               reply_to_screen_name, self, others, mutual, misc
+               )
+ntchat_tweet_sample %>% dim
+#ntchat_tweet_sample %>% write.csv("data/ntchat_tweet_sample.csv", row.names=FALSE)
+
+ntchat_sample %>% pull(user_id) %>% unique %>% length
+
+ntchat_tweeter_sample <- ntchat_no_rt %>% filter(status_id %in% sample_ids) %>%
+        distinct(user_id, .keep_all = TRUE) %>%
+        mutate(pst=0, teacher=0, te=0, consult=0, admin=0, non=0, other=0) %>%
+        select(user_id, screen_name, name, location, description, 
+               url, account_created_at, verified,
+               followers_count, friends_count, statuses_count, favourites_count,
+               pst, teacher, te, consult, admin, non, other
+               )
+ntchat_tweeter_sample %>% dim
+#ntchat_tweeter_sample %>% write.csv("data/ntchat_tweeter_sample.csv", row.names=FALSE)
+
+
+## --------------------------------------------------------------
+## Analyzing coded data
+## --------------------------------------------------------------
+
+## input coded data
+coded_tweets <- read.csv(file="data/ntchat_tweet_sample_coder1.csv", header=TRUE, colClasses='character')
+coded_tweeters <- read.csv(file="data/ntchat_tweeter_sample_coder1.csv", header=TRUE, colClasses='character')
+
+
+# margin of error for 95% confidence level
+moe <- function(x, n, z=1.96) {
+        x = x / 100
+        y = {(100 * z * sqrt(x * (1 - x) / n))} %>% round(2)
+        return(y)
+}
+
+
+## purpose of tweets
+tweet_raw <- coded_tweets %>% summarize(self = self %>% as.numeric %>% sum,
+                           others = others %>% as.numeric %>% sum,
+                           mutual = mutual %>% as.numeric %>% sum,
+                           misc = misc %>% as.numeric %>% sum
+                           )
+tweet_proportion <- (100 * tweet_raw / nrow(coded_tweets)) %>% round(2)
+tweet_moe <- tweet_proportion %>% moe(., nrow(coded_tweets))
+rbind(tweet_raw, tweet_proportion, tweet_moe)
+
+## characteristics of tweeters
+tweeter_raw <- coded_tweeters %>% summarize(pst = pst %>% as.numeric %>% sum,
+                                            teacher = teacher %>% as.numeric %>% sum,
+                             te = te %>% as.numeric %>% sum,
+                             consult = consult %>% as.numeric %>% sum,
+                             admin = admin %>% as.numeric %>% sum,
+                             grad = grad %>% as.numeric %>% sum,
+                             non.ed = non.ed %>% as.numeric %>% sum,
+                             corp = corp %>% as.numeric %>% sum,
+                             other = other %>% as.numeric %>% sum
+                           )
+tweeter_proportion <- {100 * tweeter_raw / nrow(coded_tweeters)} %>% round(2)
+tweeter_moe <- tweeter_proportion %>% moe(., nrow(coded_tweeters))
+rbind(tweeter_raw, tweeter_proportion, tweeter_moe)
+                        
+## country of tweeters
+coded_tweeters$location_manual %>% table %>% as.data.frame %>% 
+        mutate(Freq = {Freq * 100 / nrow(coded_tweeters)} %>% round(2)) %>%
+        arrange(desc(Freq))
+
+
+## --------------------------------------------------------------
+## Calculating inter-rater reliability (IRR)
+## --------------------------------------------------------------
+
+## input coded data
+coded_tweets_2 <- read.csv(file="data/ntchat_tweet_sample_coder2.csv", header=TRUE, colClasses='character')
+coded_tweeters_2 <- read.csv(file="data/ntchat_tweeter_sample_coder2.csv", header=TRUE, colClasses='character')
+
+purpose1 <- coded_tweets %>% mutate(purpose = ifelse(self=="1", "self",
+                                                     ifelse(others=="1","others", 
+                                                            ifelse(mutual=="1", "mutual", "misc")
+                                                            )
+                                                     )
+                                    ) %>% select(purpose)
+
+purpose2 <- coded_tweets_2 %>% mutate(purpose = ifelse(self=="1", "self",
+                                                       ifelse(others=="1","others", 
+                                                              ifelse(mutual=="1", "mutual", "misc")
+                                                              )
+                                                       )
+                                      ) %>% select(purpose)
+
+purpose1 %>% cbind(purpose2) %>% agree %>% `[[`(5) %>% as.numeric %>% round(2)
+purpose1 %>% cbind(purpose2) %>% kappa2 %>% `[[`(5) %>% as.numeric %>% round(2)
+
+profile1 <- coded_tweeters %>% select(pst, teacher, te, consult, admin, grad, non.ed, corp, other)
+profile2 <- coded_tweeters_2 %>% select(pst, teacher, te, consult, admin, grad, non.ed, corp, other)
+
+profile_agree <- vector(); profile_kappa <- vector()
+
+for(i in 1:9) {
+        profile_agree[i] <- profile1[,i] %>% cbind(profile2[,i]) %>% agree %>% `[[`(5) %>% as.numeric %>% round(2)
+        profile_kappa[i] <- profile1[,i] %>% cbind(profile2[,i]) %>% kappa2 %>% `[[`(5) %>% as.numeric %>% round(2)
+}
+
+irr_scores <- profile_agree %>% cbind(profile_kappa)
+rownames(irr_scores) <- c("pst", "teacher", "te", "consult", "admin", "grad", "non.ed", "corp", "other")
+colnames(irr_scores) <- c("agree", "kappa")
+irr_scores
